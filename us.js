397c36c8d37186a251d7ab39c7173fec7f91edaf
raniasalem9998 <rania.salem9998@gmail.com>
2020-05-13 22:28:02 +0300

    idont know

---
+++ us.js
@@ -0,0 +1,30 @@
+"use strict"
+var person = prompt('do you like our proj so far?', 'yes/no');
+  if (person != yes) {
+    document.getElementById("demo").innerHTML =
+    "Tanks!";
+    if (person != no) {
+        document.getElementById("demo").innerHTML =
+        "oh.. please email us why"
+    console.log(per)
+  }
+  else { NANI!}
+
+
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+  }
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
+  console.log(startTime)
